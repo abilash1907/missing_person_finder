6f2c0c86c3c6df30a153078c8dc8a24045e6b570
hari <haridinesh00@gmail.com>
2022-03-21 21:10:42 +0530

    backend

--- web/src/Pages/Register.js
+++ web/src/Pages/Register.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Register.css';
-
+import {RegionDropdown} from 'react-country-region-selector';
 function Register() {
-    
+    const [fname,SetFname]=useState('');
+    const [Lname,SetLname]=useState('');
+    const [Country,SetCountry]=useState('');
+    const [des,SetDes]=useState('');
+    const [file,setFile] = useState([])
+    const imagehandle =(e)=>{
+        for(let i =0; i<e.currentTarget.files.length;i++){
+            const newFiles= e.currentTarget.files[i]
+            newFiles["id"]=Math.random();
+            setFile(prestate=>[...prestate,newFiles])
+        }
+    }
+    const handleSubmit= e =>{
+        e.preventDefault()
+        console.log(fname);
+        console.log(Lname);
+        console.log(Country);
+        console.log(des);
+    }
+
         return (
             <div class="container">
             <h1>Register New case</h1>
-        <form action="/action_page.php">
+        <form onSubmit={handleSubmit}>
             <div class="row">
             <div class="col-25">
                 <label for="fname">First Name</label>
             </div>
             <div class="col-75">
-                <input type="text" id="fname" name="firstname" placeholder="Your name.." />
+                <input type="text" value={fname} onChange={e=> SetFname(e.currentTarget.value)} id="fname" name="firstname" placeholder="Your name.." />
             </div>
             </div>
             <div class="row">
@@ -20,7 +39,7 @@ function Register() {
                 <label for="lname">Last Name</label>
             </div>
             <div class="col-75">
-                <input type="text" id="lname" name="lastname" placeholder="Your last name.."/>
+                <input type="text" value={Lname} onChange={e=> SetLname(e.currentTarget.value)} id="lname" name="lastname" placeholder="Your last name.."/>
             </div>
             </div>
             <div class="row">
@@ -28,11 +47,8 @@ function Register() {
                 <label for="country">Last Seen</label>
             </div>
             <div class="col-75">
-                <select id="country" name="country">
-                <option value="australia">Australia</option>
-                <option value="canada">Canada</option>
-                <option value="usa">USA</option>
-                </select>
+                <RegionDropdown country="India" value={Country} onChange={e=> SetCountry(e)}/>
+
             </div>
             </div>
             <div class="row">
@@ -40,7 +56,13 @@ function Register() {
                 <label for="subject">Subject</label>
             </div>
             <div class="col-75">
-                <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
+                <textarea id="subject" value={des} onChange={e=> SetDes(e.currentTarget.value)} name="subject" placeholder="Write something.."></textarea>
+            </div>
+            <div class="col-25">
+                <label for="Image">ImageUpload</label>
+            </div>
+            <div class="col-75">
+                <input type="file" onChange={imagehandle} accept="image/*"/>
             </div>
             </div>
             <div class="row">
